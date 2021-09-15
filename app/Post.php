140b6b4a0a4ca4c83aa4abb1628fe7def58b94b3
Roberto <robertoanfuso98@gmail.com>
2021-09-15 18:49:07 +0200

    push pre pianto

--- app/Post.php
+++ app/Post.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable=[
+        'category_id',
         'title',
         'slug',
         'content'
     ];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 
 }
